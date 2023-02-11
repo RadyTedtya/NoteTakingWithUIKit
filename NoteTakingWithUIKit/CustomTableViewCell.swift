cdfe529805eaab7370430afc7595d7d68ef51314
Tedtya Rady <tedtyarady@gmail.com>
2023-02-11 15:53:57 +1100

    [Added] - WelcomeViewController
    [Action]:
    - create welcome screen

---
+++ NoteTakingWithUIKit/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  NoteTakingWithUIKit
+//
+//  Created by Tedtya rady on 11/2/2023.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
