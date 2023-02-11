41dfc859bcb11178a58fd5b0e0bc07db2ca8a647
Tedtya Rady <tedtyarady@gmail.com>
2023-02-11 14:07:58 +1100

    Initial Commit

---
+++ NoteTakingWithUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteTakingWithUIKit
+//
+//  Created by Tedtya rady on 11/2/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
